6f8ff808a2e79c1bd0530b8ea2a8135597d5176b
anthonyl <anthonyl@anthonyls-MacBook-Pro.local>
2015-04-09 18:57:31 -0500

    Initial Commit

---
+++ ChatTokenHost/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatTokenHost
+//
+//  Created by anthonyl on 4/9/15.
+//  Copyright (c) 2015 anthonyl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
